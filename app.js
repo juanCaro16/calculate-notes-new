9dbe4994137fd93b972e7c0dc39c87d392017480
Sena <dimatesebas73@gmail.com>
2024-10-02 17:51:55 -0500

    finalizacion de punto 1 inicio de punto 2

--- app.js
+++ app.js
@@ -11,6 +11,7 @@
   const data2 = document.getElementById('nota2')
   const data3 = document.getElementById('nota3')
   const btnCalculate = document.getElementById('btn-calculate')
+  const btnPredict = document.getElementById('btn-predict')
   const response = document.getElementById('resultado')
   
   btnCalculate.addEventListener('click', calculateNote )
@@ -25,8 +26,41 @@
     
     let result = ((note1 * 0.3) + (note2 * 0.3) + (note3 * 0.4)).toFixed(2)
     // template string o template literal
-    response.style.color = 'green'
-    response.textContent = `SR/SRA/SRE ${username.value} su nota definitiva es: ${result}`    
+       
+    function validarValores() {
+     if (note1<0) {
+      alert("valor invalido")
+     }
+    }
+
+  
+    function validarNotas() {
+      if (result < 3.5) {
+        
+        response.textContent = `Hola ${username.value}! perdio la materia, su nota definitiva es: ${result}`
+        response.style.color = 'black'
+            
+      }else if (result > 3.5 && result < 4.5) {
+        
+        response.textContent = `Hola ${username.value}! gano la materia, Felicidades! su nota definitiva es: ${result}`
+        response.style.color = 'orange'
+      }else if (result >4.5) {
+        
+        response.textContent = `Hola ${username.value}! Gano la materia, Felicidades Fue sobresaliente!, Su nota definitiva es : ${result}`
+        response.style.color = 'green'
+      }
+    }
+
+    btnPredict.addEventListener('click', predecirNotas)
+
+    function predecirNotas() {
+
+        
+
+    }
+    validarValores()
+    validarNotas()
+    predecirNotas()
   }
 
   
